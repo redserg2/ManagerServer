2eb920618078f8630e2acba0a25e119fa60829fc
redserg <redserg@land.ru>
2016-09-07 02:25:27 +0300

    singletone database context

--- ManagerBL/IManagerDataAccess.cs
+++ ManagerBL/IManagerDataAccess.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ManagerBL
 {
+    public enum DataAccessResult
+    {
+        SUCCESS,
+        EXISTING_RECORD,
+        INVALID_ARGUMENTS,
+        CONNECTION_ERROR
+    }
+
     public interface IManagerDataAccess
     {
-        
+        DataAccessResult AddUser(UserInfo user_info);
+
     }
 }
---
+++ ManagerBL/UserInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerBL
+{
+    class UserInfo
+    {
+        public String LoginName{get;set;}
+
+        public String FirstName { get; set; }
+
+        public String Address { get; set; }
+
+        public String PhoneNumber { get; set; }
+
+        public String EMail { get; set; }
+    }
+}
